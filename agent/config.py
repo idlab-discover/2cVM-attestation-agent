03f0b542de7a595290ed4545031a7389e360cd93
Jordi Thijsman <jordi.thijsman@ugent.be>
2025-01-07 15:38:13 +0100

    fix path bug

--- agent/config.py
+++ agent/config.py
@@ -4,7 +4,7 @@ import os
 SEV_SNP_ENABLED = False  # Mock platform report for debuggin on non SEV-SNP systems
 HOME_DIR = os.path.expanduser("~")
 AGENT_DIR = os.path.join(HOME_DIR, ".attestation-agent")
-API_VERSION = "V1"
+API_VERSION = "v1"
 
 # routes/application
 APPLICATION_API_PATH = os.path.join("/", API_VERSION, "application")
@@ -17,7 +17,7 @@ COMPOSITE_WASM_FILE = os.path.join(COMPONENT_FOLDER, "composite.wasm")
 WASM_OUTPUT_DIR = os.path.join(AGENT_DIR, "output")
 
 # routes/attestation
-ATTESTATION_API_PATH = APPLICATION_API_PATH = os.path.join(
+ATTESTATION_API_PATH = os.path.join(
     "/", API_VERSION, "attestation")
 SNP_GUEST_BIN_FILE = os.path.join(HOME_DIR, "snpguest")
 KEY_FOLDER = os.path.join(AGENT_DIR, "keys")
@@ -25,10 +25,10 @@ PRIVATE_KEY_FILE = os.path.join(KEY_FOLDER, "private_key.pem")
 PUBLIC_KEY_FILE = os.path.join(KEY_FOLDER, "public_key.pem")
 
 # routes/lock
-LOCK_API_PATH = APPLICATION_API_PATH = os.path.join("/", API_VERSION, "lock")
+LOCK_API_PATH = os.path.join("/", API_VERSION, "lock")
 LOCK_FOLDER = os.path.join(AGENT_DIR, "lock")
 LOCK_FILE = os.path.join(LOCK_FOLDER, "commitment-manifest.json")
 
 # routes/status
-STATUS_API_PATH = APPLICATION_API_PATH = os.path.join(
+STATUS_API_PATH = os.path.join(
     "/", API_VERSION, "status")
